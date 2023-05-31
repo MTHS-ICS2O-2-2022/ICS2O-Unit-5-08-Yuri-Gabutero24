d9174a23120b6d76897b6e94af1c294d0565ea8c
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-31 17:24:38 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  //input
+  const numberOne = parseInt(document.getElementById("firstNumber").value)
+  const numberTwo = parseInt(document.getElementById("secondNumber").value)
+
+  // process
+  let counter = 0
+  let answer = 0
+  let remainder = numberOne
+  while (remainder >= numberTwo) {
+    remainder -= numberTwo
+    counter++
+  }
+  answer = counter
+
+  //output
+  if remainder = 0 {
+  document.getElementById("answer").innerHTML = numberOne + " / " + numberTwo + " = " + answer + " with a remainder of " + remainder
+  } else {
+  document.getElementById("answer").innerHTML = numberOne + " / " + numberTwo + " = " + answer
+  }
+}
